974e0a0145c6a19ce2742e76a675de96154bc0da
Иван Бонькин <i.bonkin@pflb.ru>
2017-09-26 17:13:21 +0300

    added change owner name test

--- src/test/java/ru/pflb/wd/PetclinicTest.java
+++ src/test/java/ru/pflb/wd/PetclinicTest.java
@@ -1,9 +1,12 @@
 package ru.pflb.wd;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,6 +16,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.openqa.selenium.firefox.FirefoxDriver.PROFILE;
 
 /**
@@ -47,12 +51,57 @@ public class PetclinicTest {
      */
     @Test
     public void shouldFindOwnerAndChangeHisName() {
+        final String surname = "Black";
 
-        driver.get("http://localhost:8080");
+        // заход на главную страницу
+        driver.get("http://localhost:8080/");
+
+        // клик по меню Find Owners
+        driver.findElement(By.xpath("//a[@title='find owners']")).click();
+
+        // ввод фамилии клиента (Black) в поле поиска
+        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys(surname);
+
+        // клик кнопки Find Owner
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        // клик кнопки Edit Owner
+        driver.findElement(By.xpath("//h2[.='Owner Information']/following-sibling::a[starts-with(., 'Edit')]")).click();
+
+        // генерация произвольного имени из 6 букв
+        String newClientName = RandomStringUtils.randomAlphabetic(6);
+
+        // ввод сгенерированного имени в поле First Name
+        WebElement textField = driver.findElement(By.xpath("//input[@id='firstName']"));
+        textField.clear();
+        textField.sendKeys(newClientName);
+
+        // запоминание содержимого полей Address, City, Telephone
+        String address = driver.findElement(By.xpath("//input[@id='address']")).getAttribute("value");
+        String city = driver.findElement(By.xpath("//input[@id='city']")).getAttribute("value");
+        String telephone = driver.findElement(By.xpath("//input[@id='telephone']")).getAttribute("value");
+
+        // нажатие кнопки Update Owner
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        // проверки содержимого таблицы Owner Information
+        assertThat(driver.findElement(By.xpath("//th[.='Name']/following-sibling::td/b")).getText())
+                .describedAs("Измененное имя пользователя не совпадает со сгенерированным")
+                .isEqualTo(newClientName + " " + surname);
+        assertThat(driver.findElement(By.xpath("//th[.='Address']/following-sibling::td")).getText())
+                .describedAs("Адрес пользователя не совпадает с первоначальным")
+                .isEqualTo(address);
+        assertThat(driver.findElement(By.xpath("//th[.='City']/following-sibling::td")).getText())
+                .describedAs("Город пользователя не совпадает с первоначальным")
+                .isEqualTo(city);
+        assertThat(driver.findElement(By.xpath("//th[.='Telephone']/following-sibling::td")).getText())
+                .describedAs("Телефон пользователя не совпадает с первоначальным")
+                .isEqualTo(telephone);
+
     }
 
     /**
-     * Найти пользователя по фамилии Black и добавить ему домашнее животное
+     * Найти пользователя по фамилии Black и добавить ему домашнее животное.
      */
     @Test
     public void shouldAddNewPet() {
@@ -69,6 +118,8 @@ public class PetclinicTest {
      * <li>Нажать Add Owner, открылась страница Owner Information</li>
      * <li>Проверить, что добавилась новая запись, и все ее поля соответствуют введенным значениям, использую поиск в Find Owners (заново)</li>
      * </ul>
+     *
+     * Условие - не использовать индексы в XPath без крайней на то необходимости
      */
     public void shouldValidateAddedUser() {
         // TODO
